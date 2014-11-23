486093713446cb688533411522b7599d133de514
James Awesome <jamestheawesome@gmail.com>
2014-11-23 17:05:43 -0500

    Add version number to config

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
+    VERSION = '0.0.1'
     DEBUG = True
     DEFAULT = True
     RESUME_JSON = os.environ.get('RESUME_JSON')
