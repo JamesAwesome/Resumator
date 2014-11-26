88f2d3ba2a4508071081e1fb6e6d3bf783d6b27e
James Awesome <jamestheawesome@gmail.com>
2014-11-25 22:54:44 -0500

    Bump version & update requirements

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    VERSION = '0.1.2'
+    VERSION = '0.2.0'
     DEBUG = True
     DEFAULT = True
     RESUME_JSON = os.environ.get('RESUME_JSON')
